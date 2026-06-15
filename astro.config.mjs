f49e94e2580edb25e1d6d0e2a880f6e9a9e5863d
paul lahana <paul.lahana@gmail.com>
2026-06-15 09:59:27 +0200

    Add Sentry integration for error tracking and update dependencies in configuration files

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,25 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
+import sentry from '@sentry/astro';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://meridian-way.ch',
-  integrations: [
-    sitemap({
-      i18n: {
-        defaultLocale: 'fr',
-        locales: {
-          fr: 'fr',
-          de: 'de',
-          it: 'it',
-          en: 'en',
-        },
+  integrations: [sitemap({
+    i18n: {
+      defaultLocale: 'fr',
+      locales: {
+        fr: 'fr',
+        de: 'de',
+        it: 'it',
+        en: 'en',
       },
-    }),
+    },
+  }), sentry({
+      project: "meridian-way",
+      org: "meridian-way",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    })
   ],
 });
